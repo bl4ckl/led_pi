c29d138a6dc90a4fce832c60acb9e54962acde5a
bl4ck <bl4ck@posteo.net>
2018-11-04 16:01:35 +0000

    removed message definitions

--- networking/tcpclient.h
+++ networking/tcpclient.h
@@ -1,16 +1,6 @@
 #ifndef TCPCLIENT_H
 #define TCPCLIENT_H
 
-#define MESSAGE_ID 		0
-#define MESSAGE_CONFIG 		1
-#define MESSAGE_EFFECTS 	2
-#define MESSAGE_TIME		3
-#define MESSAGE_PLAY		4
-#define MESSAGE_PAUSE		5
-#define MESSAGE_PREVIEW		6
-#define MESSAGE_SHOW		7
-#define MESSAGE_COLOR		8
-
 extern int tcpcreatesocket(int* __tcpsockfd);
 
 extern int tcpsetuphandler(int __tcpsockfd, void* __handler_function);
